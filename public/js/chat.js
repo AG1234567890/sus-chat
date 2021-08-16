6a2cb60ea3288d664764252aae24e9c8fc2285a4
AG1234567890 <49893607+AG1234567890@users.noreply.github.com>
2021-08-16 10:57:54 -0700

    Init commit unbirth

---
+++ public/js/chat.js
@@ -0,0 +1,95 @@
+const socket = io();
+// const { EmojiButton } = require ('@joeattardi/emoji-button');
+const $messageForm = document.querySelector("#message-form");
+const $messageFormInput = $messageForm.querySelector("input");
+const $messageFormButton = $messageForm.querySelector("button");
+const $sendLocationButton = document.querySelector("#send-location");
+const $messages = document.querySelector("#messages");
+
+//Templates
+const messageTemplate = document.querySelector("#message-template").innerHTML;
+const locationMessageTemplate = document.querySelector(
+  "#location-message-template"
+).innerHTML;
+
+//Options
+
+const { username, room } = Qs.parse(location.search, {
+  ignoreQueryPrefix: true,
+});
+
+// const picker = new EmojiButton();
+// const trigger = document.querySelector('.trigger');
+
+// picker.on('emoji', selection => {
+//   // `selection` object has an `emoji` property
+//   // containing the selected emoji
+// });
+
+// trigger.addEventListener('click', () => picker.togglePicker(trigger));
+
+socket.on("message", (message) => {
+  console.log(message);
+  const html = Mustache.render(messageTemplate, {
+    message: message.text,
+    createdAt: moment(message.createdAt).format("h:mm A"),
+  });
+  $messages.insertAdjacentHTML("beforeend", html);
+});
+
+socket.on("locationMessage", (message) => {
+  console.log(message);
+  const html = Mustache.render(locationMessageTemplate, {
+    url: message.url,
+    createdAt: moment(message.createdAt).format("h:mm A"),
+  });
+  $messages.insertAdjacentHTML("beforeend", html);
+});
+
+$messageForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  $messageFormButton.setAttribute("disabled", "disabled");
+
+  const message = e.target.elements.message.value;
+
+  socket.emit("sendMessage", message, (error) => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log("Message sent");
+  });
+  $messageFormButton.removeAttribute("disabled");
+  $messageFormInput.value = "";
+  $messageFormInput.focus();
+});
+
+$sendLocationButton.addEventListener("click", () => {
+  if (!navigator.geolocation) {
+    return alert("Geolocation is not supported by your browser");
+  }
+
+  $sendLocationButton.setAttribute("disabled", "disabled");
+
+  navigator.geolocation.getCurrentPosition((position) => {
+    socket.emit(
+      "sendLocation",
+      {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+      },
+      () => {
+        $sendLocationButton.removeAttribute("disabled");
+        console.log("Location shared");
+      }
+    );
+  });
+});
+
+socket.emit("join", { username, room}, (error) => {
+  if(error) {
+    alert(error)
+    location.href="/"
+  }
+
+});
